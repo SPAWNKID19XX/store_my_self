88145af0accbc7b5fa3f0506ed2063300718e87f
PERSONAL_BORIS <borisisac@gmail.com>
2025-03-30 18:57:01 +0100

    [ADD]app main

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
 
     # Local apps
     'users.apps.UsersConfig',
+    'main.apps.MainConfig',
 ]
 
 
---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import Welcome from '../src/components/welcome.jsx';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>Моё React-приложение</h1>
+      <Welcome name="друг" />
+    </div>
+  );
+}
+
+export default App
